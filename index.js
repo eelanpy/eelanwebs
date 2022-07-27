fb074233f6c57217feb5ba0f017cbf38c9e2f33f
eelanpy <76450488+eelanpy@users.noreply.github.com>
2022-07-27 10:37:48 -0400

    Add files via upload
    made home pages not in one folder just in same folder (Also, did some styling if user uses web on phone like padding, zooming out), and for the movies website, made the spinner when getting data from api.

---
+++ index.js
@@ -0,0 +1,23 @@
+const formSignin = document.querySelector('.container');
+
+formSignin.innerHTML = '';
+
+function addButton(game, link) {
+	const btnGame = document.createElement('a');
+	btnGame.classList.add('btn');
+	btnGame.classList.add('btn-primary');
+	btnGame.classList.add('text-center');
+	btnGame.classList.add('btn-lg');
+	btnGame.classList.add('mt-2');
+	btnGame.href = link;
+	btnGame.textContent = game;
+	formSignin.appendChild(btnGame);
+}
+
+addButton('Movie Info', 'movie/index.html')
+addButton('Multiplication Quiz', 'multiplication/multiplication.html')
+addButton('Number Guessing', 'numberguess/index.html')
+addButton('Weather', 'weatherweb/index.html')
+addButton('Wordscape Cheatsheet', 'wordscape/wordscape.html')
+addButton('Hangman Cheatsheet', 'hangman/hangman.html')
+addButton('Covid Information in YRDSB', 'https://www.eelanpy.com/')
